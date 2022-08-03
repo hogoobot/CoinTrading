bc60b64bc1d76f0a3fa9096d33812a80cb16dae5
hogoobot <ldw159@naver.com>
2022-08-03 20:34:29 +0900

    first upload

---
+++ webscraping.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_per(code):
+    url = "https://finance.naver.com/item/main.nhn?code=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, "html5lib")
+    tags = soup.select("#_per")
+    tag = tags[0]
+    return float(tag.text)
+
+def get_dividend(code):
+    url = "https://finance.naver.com/item/main.nhn?code=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, "html5lib")
+    tags = soup.select("#_dvr")
+    tag = tags[0]
+    return float(tag.text)
+
+url = "https://finance.naver.com/item/main.nhn?code=000660"
+html = requests.get(url).text
+
+soup = BeautifulSoup(html, "html5lib")
+tags = soup.select("#tab_con1 > div:nth-child(3) > table > tbody > tr.strong > td > em")
+
+for tag in tags:
+    print(tag.text)
